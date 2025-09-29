1a25c97b1be885fbe5f81bfc3cc73d268a35fda9
Its-JustSbu <116superstm@gmail.com>
2025-09-29 16:42:38 +0200

    wholesale

--- backend/api/app.js
+++
@@ -1,8 +0,0 @@
-import app from "../server.js";
-import scriptures from "./scriptures.js";
-import users from "./users.js";
-
-app.use('/scriptures', scriptures);
-app.use('/users', users);
-
-export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import cors from 'cors';
-import scriptures from './routes/scriptures.js';
-import users from './routes/users.js';
-import { config }  from 'dotenv';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cors from "cors";
+import scriptures from "./routes/scriptures.js";
+import users from "./routes/users.js";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
 
 config();
 
@@ -11,12 +11,17 @@ const app = express();
 
 const { PORT } = process.env || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('Scripture App API');
-})
+app.get("/", (req, res) => {
+  res.send("Scripture App API");
+});
 
 export default app;
